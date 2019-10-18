76abbbd4186c7fa2c4d32a96d2679d225748ad0a
libra <libraxiang@outlook.com>
2019-10-18 16:59:13 +0800

    创建项目的基本结构

---
+++ Main.js
@@ -0,0 +1,8 @@
+// 主程序：初始化游戏过程中的数据 ，以及点击事件的绑定
+
+//  暴露
+export class Main{
+    constructor() {
+        console.log("Game Start");
+    }
+}
---
+++ game.js
@@ -0,0 +1,6 @@
+//  小游戏的入口程序
+
+// 引入 Main 模块
+import  {Main}  from "./Main.js";
+
+new Main();
